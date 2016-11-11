65c9c21793b12cafa2eaa45731f058d6390398ab
jc781 <jc781@exeter.ac.uk>
2016-11-11 12:38:55 +0000

    Added documentation and added an equals method for use in testing etc

--- Card.java
+++ Card.java
@@ -1,13 +1,39 @@
-package threadingca;
+package game;
 
-public class Card {
-    private final int value;
-    
-    Card(int num){
-        this.value = num;    
+/**
+ * Card class
+ * @version 1.2
+ */
+public class Card{
+    private int value;
+
+    /**
+     * Constructor for a Card
+     * @param value         cards face value
+     */
+    public Card(int value){
+        this.value = value;
+    }
+
+    /**
+     * Method gets the cards value
+     * @return              cards face value
+     */
+    public int getValue(){
+        return value;
     }
     
-    int getValue(){
-        return this.value;
+    @Override
+    /**
+     * Method compares an object for equivalency with the card instance
+     * @param obj           object to be tested for equivalency with card instance
+     * @return              true if object is equivalent, else false
+     */
+    public boolean equals(Object obj){
+        if (obj instanceof Card){
+	   if(this.value == ((Card) obj).value)
+                return true;
+        }
+        return false;
     }
 }

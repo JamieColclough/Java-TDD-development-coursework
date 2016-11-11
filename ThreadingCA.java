207d8a1657652d03ba14ca72a5d8de2082d6c402
jc781 <jc781@exeter.ac.uk>
2016-11-11 12:32:12 +0000

    Changes name of main class to Game.java as well as creating new functions for dealing out the cards from the initial pack and adding comments and documentation

--- ThreadingCA.java
+++ ThreadingCA.java
@@ -1,4 +1,4 @@
-package threadingca;
+package game;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -6,17 +6,57 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
- *ThreadingCA application class
- *@ version 1.2
+ *Game application class
+ *@ version 1.3
  */
-
-public class ThreadingCA {
-
-    /**
-     * @param args the command line arguments
-     * @throws java.io.FileNotFoundException
+public class Game {
+    
+    /** Function to deal out cards in a round robin fashion to either form all hands or all decks
+     * 
+     * 
+     * @param pack the initial pack that deals out all cards
+     * @param isHand boolean to indicate whether or not to deal with respect to a hand or deck
+     * @param n amount of players
+     * @param placeInPack indexing starts from different place depending on deck or card
+     * @return Array of either decks or hands
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    static Card[][] deal(Card[] pack, boolean isHand, int n, int placeInPack){
+        Card[][] cardsArray = isHand? new Card[n][4]: new Card[n][4*n]; //if hand: each array is size 4 else size 4n
+                
+        int addCount = placeInPack; //Starting index to iterate through deck array
+        for(int cIndex=0; cIndex<4; cIndex++){ //deals to hand or deck in a round robin fashion
+            for (int pIndex=0; pIndex<n; pIndex++ ){
+                cardsArray[cIndex][pIndex] = pack[addCount];
+                addCount++; //increments count so next card added is next one in the deck
+            }
+        }      
+        return cardsArray;
+    }
+  
+    /**Static function to return all hands that will be used in the game
+     * 
+     * @param pack the initial pack that deals out all cards
+     * @param n the amount of players
+     * @return an array of hands
+     */
+    static Card[][] dealToHands(Card[] pack, int n){
+        return deal(pack, true, n, 0); //sets parameters to specify hand in the deal function
+    }
+
+    /** Static function to return all decks that will be used in the game
+     * 
+     * @param pack the initial pack that deals out all cards
+     * @param n the amount of players
+     * @return an array of decks
+     */
+    static Card[][] dealToDecks(Card[] pack, int n){
+        return deal(pack, false, n, 4*n);
+    }
+    
+    /**Static Main method to ask user for input and simulate the game
+     * @param args the command line arguments
+     */   
+    public static void main(String[] args){
         Scanner cmdScan = new Scanner(new InputStreamReader(System.in));
         
         //While loop guard to repeatedly query user until correct file given
@@ -24,17 +64,18 @@ public class ThreadingCA {
         while ((n < 2) || (n > 7)){
             System.out.println("Enter Value of n (Must be Integer between 2 and 7):");
             try {
-                 n = Integer.parseInt(cmdScan.next()); //if n isn't an integer
+                 n = Integer.parseInt(cmdScan.next()); //if n isn't an integer will catch and ask again
             }catch(NumberFormatException e){System.out.println("Invalid type of n, Must be int");}
         
         }
         
-        
+        //guarded while loop to query user for a legitimate text file
         String textFile;
         FileReader fr = null;
         while (fr == null) {       
             System.out.println("text file:  "); 
             try{
+                //Must be in full path form i.e. C:\sample_deck.txt
                 textFile = cmdScan.next();       
                 fr = new FileReader(textFile);
             }catch(FileNotFoundException e){System.out.println("File Not Found");}
@@ -51,28 +92,8 @@ public class ThreadingCA {
         } 
         
         //iterates over handArray, distributing each card to it in a round robin fashion
-        Card[][] handArray = new Card[n][4];
-        
-        addCount = 0;
-        for(int hIndex=0; hIndex<4; hIndex++){
-            for (int pIndex=0; pIndex<n; pIndex++ ){
-                handArray[pIndex][hIndex] = initialPack[addCount];
-                addCount++;
-            }
-            }   
+        Card[][] handArray = dealToHands(initialPack, n);
         
         //performs same iterations but this time on the decks in a round robin fashion
-        Card[][] deckArray =  new Card[n][4*n];
-        
-        addCount = 0;
-        for(int hIndex=0; hIndex<4; hIndex++){
-            for (int pIndex=0; pIndex<n; pIndex++ ){
-                deckArray[pIndex][hIndex] = initialPack[addCount];
-                addCount++;
-            }
-        }
-        
-        
-        
-    }
-    }
+        Card[][] deckArray =  dealToDecks(initialPack, n);
+    }}        

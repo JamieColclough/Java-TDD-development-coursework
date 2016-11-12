2b0610bf718147378244f49798008faa1c2a109f
jc781 <jc781@exeter.ac.uk>
2016-11-12 15:28:49 +0000

    Made main method run the threads to simulate the game, also added newer documentation

--- Game.java
+++ Game.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *Game application class
- *@ version 1.4 
+ *@ version 1.5
  */
 public class Game {
     
@@ -32,7 +32,12 @@ public class Game {
             else if (fileLength > 8*n){System.out.println("File too large, using first " + 8*n + " Cards");return true;}
             else {return true;}
         }
-    
+    /**
+     * Method to fill initialPack from text file
+     * @param fr fileReader of text file that is proven to already obey the rules of the game
+     * @param n the amount of players in the game
+     * @return the pack filled with card instances
+     */
     static Card[] fillPack(FileReader fr,int n ){
         Scanner fileScan = new Scanner(fr);
         Card[] initialPack = new Card[8*n];
@@ -42,15 +47,20 @@ public class Game {
         }
         return initialPack;
     }
-    
+    /**
+     * Function to see if game is able to be won i.e. the game will not stagnate indefinitely
+     * @param pack the initial pack
+     * @param n the number of players
+     * @return boolean corresponding to whether the game can be won
+     */
     static boolean winnableGame(Card[] pack,int n){
         int counter;
         for (int i=0; i<8*n;i++){
             counter = 0;
             for(int j=0; j<8*n;j++){
                 if (pack[i].getValue() == pack[j].getValue()) counter++; 
-            }
-            if (counter >= 4) return true;
+            }//counter done for each member of the list
+            if (counter >= 4) return true;//if there is no member that appears more than 4 times,game can't be won
         }
         System.out.println("Deck doesn't have more than 3 of a single card, impossible to win");
         return false;   
@@ -59,7 +69,7 @@ public class Game {
     
     
     
-    /** Function to deal out cards in a round robin fashion to either form all hands or all decks 
+    /** Function to deal out cards in a round robin fashion to either form all hands or all decks     * 
      * 
      * @param pack the initial pack that deals out all cards
      * @param isHand boolean to indicate whether or not to deal with respect to a hand or deck
@@ -143,18 +153,28 @@ public class Game {
             
         gameReady = winnableGame(initialPack,n); //once pack created,checks to see if the game is able to finish
         //closes scanners 
-        fr.close(); 
-        
+        fr.close();     
         
     }
         
         cmdScan.close();  //closes fileReaders and scanners to avoid inteference with the threading
+        Thread[] playerArray = new Thread[n];
         
         //iterates over handArray, distributing each card to it in a round robin fashion
         Card[][] handArray = dealToHands(initialPack, n);
-
-
+        
         //performs same iterations but this time on the decks in a round robin fashion
         Card[][] deckArray =  dealToDecks(initialPack, n);
+        
+        CardDeck[] cardDecks = new CardDeck[n];        
+        for (int decks=0;decks<n;decks++){cardDecks[decks] = new CardDeck(deckArray[decks]);}
+        for (int q=0; q<n;q++){
+            //z made to find 0th member of array for right deck when lightDeck is n-1
+            int z = (q == n-1)?0:q+1;
+            playerArray[q] = new Thread (new Player((q+1),handArray[q],cardDecks[q],cardDecks[z]));
+        }
+        
+        for (Thread player: playerArray){player.start();}
 
+    }
     }

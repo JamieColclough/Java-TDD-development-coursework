08e172093148b9f473844ff2672cc485fbdd998b
jc781 <jc781@exeter.ac.uk>
2016-11-14 00:08:47 +0000

    subdivided tests to see more clearly what is being tested and improving readability

--- GameTest.java
+++ GameTest.java
@@ -2,6 +2,7 @@ package game;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -27,6 +28,9 @@ public class GameTest {
     FileReader fr1;
     FileReader fr2;
     FileReader fr3;
+    
+    int testHandValue;
+    int testDeckValue;
     
    @Rule
    public TemporaryFolder tempFolder = new TemporaryFolder(); //sets up temporary folder, holds temp files and deletes itself after end of test
@@ -63,32 +67,47 @@ public class GameTest {
     }
 
     /**
-     * Test of correctFile method, of class ThreadingCA.
-     * @throws java.io.IOException
+     * Tests that correctFile works in normal circumstances i.e. the file is legitimate
+     * @throws java.io.FileNotFoundException
      */
     @Test
-    public void testCorrectFile() throws IOException{
+    public void testCorrectFileNormalFile() throws FileNotFoundException{
         //first 3 tests based on file size, tests for when file is exactly right,too small and too big
         assertTrue(Game.correctFile(fr1,3));
         fr1= new FileReader(file1);
         assertTrue(Game.correctFile(fr1,4));
         fr1 = new FileReader(file1);
         assertFalse(Game.correctFile(fr1,5));
-        
-        //covers corner cases of an empty file and a file not containing integers
-            assertFalse(Game.correctFile(fr2, 2));
-            assertFalse(Game.correctFile(fr3, 2));
-            fr2 = new FileReader(file2);
-            fr3 = new FileReader(file3);
-        
-        
+    }
+    /**
+     * Tests that correctFile works when the file is empty
+     * @throws FileNotFoundException 
+     */
+    @Test
+    public void testCorrectFileEmpty() throws FileNotFoundException{
+        //covers corner cases of an empty file 
+        assertFalse(Game.correctFile(fr2, 2));            
+        fr2 = new FileReader(file2);
+                
     }
     
+    /**
+     * Tests that correctFile works when file has non-integers present
+     * @throws FileNotFoundException 
+     */
     @Test
-    public void testWinnableGame(){
-        Card[] invalidCardArray = new Card[24];
-        Card[] validCardArray = new Card[24];
-        
+    public void testCorrectFileInvalid() throws FileNotFoundException{
+        //covers corner case of a file not containing integers
+        assertFalse(Game.correctFile(fr3, 2)); 
+        fr3 = new FileReader(file3);
+    }
+    
+    /**
+     * Tests that when when a file is valid, winnableGame returns false
+     */
+    @Test
+    public void testWinnableGameValid(){
+        Card[] validCardArray = new Card[24];        
         
         int cardCount = 0;
         for (int i=0; i<4; i++){
@@ -96,16 +115,23 @@ public class GameTest {
                 validCardArray[cardCount] = new Card(j);
                 cardCount++;
             }
-        }
+        }        
+        assertTrue(Game.winnableGame(validCardArray, 3));
         
-        cardCount = 0;
+    }
+    /**
+     * Tests that when a game isn't winnable, winnableGame returns false
+     */
+    @Test
+    public void testWinnableGameInvalid(){
+        Card[] invalidCardArray = new Card[24];
+        int cardCount = 0;
         for (int i=0; i<3; i++){
             for (int j=0;j<8;j++){
                 invalidCardArray[cardCount] = new Card(j);
                 cardCount++;
             }
         }
-        assertTrue(Game.winnableGame(validCardArray, 3));
         assertFalse(Game.winnableGame(invalidCardArray,3));
     }
     
@@ -113,33 +139,34 @@ public class GameTest {
      * Checks the deal function splits the decks in a round robin fashion
      */
     @Test
-    public void testDeal() {        
-        for(int n = 2; n<8;n++){
+    public void testDeal() {
         int x = 1;
-        //creates the deck to be tested on the method, in the form 1,2,....n n times over
-            Card[] deck = new Card[8*n];
-               for (int i=1; i<=8*n; i++){       
-                   if (x>n) x=1;
+        //n=4 chosen for this test
+            Card[] deck = new Card[32];
+               for (int i=1; i<=32; i++){       
+                   if (x>4) x=1;
                    deck[i-1] = new Card(x);
                    x++;
                 }
                
-                //tests both dealHand and Deal deck so testing those functions seperately is unecessary
-                Card[][] dealedHand = Game.deal(deck, true,n,0);
-                Card[][] dealedDeck = Game.deal(deck, false, n, 4*n);
+            //tests both dealHand and Deal deck so testing those functions seperately is unecessary
+            Card[][] dealedHand = Game.deal(deck, true,4,0);
+            Card[][] dealedDeck = Game.deal(deck, false, 4, 16);
                 
-                for (int j=0;j<n;j++){                    
-                    for (int m=0; m<4; m++){
-                        assertEquals(dealedHand[j][m].getValue(),j+1);
-                        assertEquals(dealedDeck [j][m].getValue(),j+1);
-                    }
+            for (int j=0;j<4;j++){                    
+                for (int m=0; m<4; m++){
+                    testHandValue = dealedHand[j][m].getValue();
+                    testDeckValue = dealedDeck[j][m].getValue();
+                    assertEquals(j+1,testHandValue);
+                    assertEquals(j+1,testDeckValue);
                 }
+            }
         }
                 
-    }
+    
 
     /**
-     * Test of fillPack method, of class Game.
+     * Tests that fillPack fills a pack in the correct fashion
      */
     @Test
     public void testFillPack() {

ca10185d4cc41b3d5d808351ab082c5b6064a2a0
jc781 <jc781@exeter.ac.uk>
2016-11-15 10:50:04 +0000

    Edited inconsistinces and updated documents

--- Player.java
+++ Player.java
@@ -3,7 +3,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.Arrays;
 /**
  * Player implements runnable interface
- * @version 1.3
+ * @version 1.4
  */
 public class Player implements Runnable
 {
@@ -14,15 +14,15 @@ public class Player implements Runnable
     private final static AtomicBoolean ALL_ALIVE = new AtomicBoolean(true);
     // AtomicBoolean used so that only one player thread can read/write at a time,
     // again this allows only most up to date value to be seen.    
-    private Card[] hand = new Card[5];  // 5 becasue rules specify you take a card before placing card
+    private final Card[] hand = new Card[5];  // 5 becasue rules specify you take a card before placing card
     private int cardsInHand = 0;
-    private int preference;
-    private CardDeck leftDeck;
-    private CardDeck rightDeck;
+    private final int preference;
+    private final CardDeck leftDeck;
+    private final CardDeck rightDeck;
 
     /**
-     * Consturctor for a Player
-     * @param preference        the players perfered number, also the players number
+     * Constructor for a Player
+     * @param preference        the players preferred number, also the players number
      * @param cards             cards dealt to player, the players hand
      * @param leftDeck          deck of cards the player will take a card from
      * @param rightDeck         deck of cards the player will place a card on
@@ -37,7 +37,7 @@ public class Player implements Runnable
         for(int i=0; i<4; i++)
         {
             // while loop is used to remove 'gaps' where null entries may occur between card entries
-            // in a card array. - this wont happen in our game, but could happen if the class was to
+            // in a card array. - this won't happen in our game, but could happen if the class was to
             // to be used in a different context.
             if(cards[i] != null)
             {
@@ -58,20 +58,20 @@ public class Player implements Runnable
     public boolean addCard(Card card)
     // needed to add a single card to the hand once it has been taken from the left card deck  
     {
-        if (cardsInHand == 5){return false;} // cant add anymore cards, hand is full
+        if (cardsInHand == 5){return false;} // can't add anymore cards, hand is full
         hand[cardsInHand] = card;
         cardsInHand++;
         return true;     
     }
     
     /**
-     * Method checks if the players current hand is a winning hand where all
+     * Method checks if the player's current hand is a winning hand where all
      * cards have the same value
      * @return          true if hand is a winning hand, else false
      */
     public boolean winningHand()
     // this method compares card values in the hand to the first cards value, if they all have the same
-    // value it return true
+    // value it returns true
     {
         int value = hand[0].getValue();
         boolean winningHand = true;
@@ -88,17 +88,17 @@ public class Player implements Runnable
 
     /**
      * Method returns first card whose value is not the same as the players preference
-     * @return          non prefered card, or null if no such card was found
+     * @return          non preferred card, or null if no such card was found
      */
     public Card nonPreferedCard()
     {
         // should be called after winningHand() check, if the hand is a 'winning hand' consisting of cards
-        // that are the players preference this method will return null. A player cannot give a prefered
+        // that are the players preference this method will return null. A player cannot give a preferred
         // card if there are none.
         Card nonPrefered = null;
         
-        if (!winningHand()){ // checks that there are non prefered cards before trying to take one.
-            int index= 0; // index of non-prefered
+        if (!winningHand()){ // checks that there are non preferred cards before trying to take one.
+            int index= 0; // index of non-preferred
             for (int i=0; i<cardsInHand; i++)
             {
                 if (hand[i].getValue() != preference)
@@ -111,8 +111,8 @@ public class Player implements Runnable
         
             hand[index] = null;
             for(int i = index; i<=(cardsInHand - 2); i++)
-            { // shuffles cards down after the space the non prefered card is taken from is set to null
-              // it stops nullPointerExceptions from being thrown when doing winningHand() checks.
+            { // shuffles cards down after the space the non preferred card is taken from is set to null
+              // it stops NullPointerExceptions from being thrown when doing winningHand() checks.
                 hand[i] = hand[i+1];
                 hand[i+1] = null;
             }
@@ -129,7 +129,7 @@ public class Player implements Runnable
      */
     public boolean equals(Object obj)
     // used so that assertEquals() used in junit testing has an accurate equals method to call when
-    // assert that players are the same, this judges players to be the same if they are in the same 
+    // asserting that players are the same, this judges players to be the same if they are in the same 
     // state, (so same card decks and same hand) not just same player number/preference.
     {
         if(obj instanceof Player){
@@ -146,8 +146,9 @@ public class Player implements Runnable
     
     /**
      * Method allows player to run and play the game, loop is repeated until a winner
-     * is found, a different player is interrupted, or this player is intertupted
+     * is found, a different player is interrupted, or this player is interrupted
      */
+    @Override
     public void run()
     {
         System.out.println("Player " + preference + " started run");
@@ -158,19 +159,19 @@ public class Player implements Runnable
             if(WINNER.compareAndSet(false,winningHand()))
             // compareAndSet used as it combines an atomic read and write, so that only one player
             // at a time can read the WINNER value and change the value at time. as the reading 
-            // and writting are carried out sychronously in compareAndSet(), only one player is able to
-            // declare its the winner.
+            // and writing are carried out sychronously in compareAndSet(), only one player is able to
+            // declare it's the winner.
                 System.out.println("Player " + preference + " wins");
         }           
 
         while (!WINNER.get() && ALL_ALIVE.get()&& !Thread.currentThread().isInterrupted()) 
         // will stop running if a winner found, or not all threads
         // are alive, meaning a players thread was interrupted
-        // terminated
+        // or terminated
         {
             try{        
 
-                // if a another player has been interrupted, there is the possiblity that this player
+                // if another player has been interrupted, there is the possiblity that this player
                 // could get stuck waiting in takeCard 
                 
                 addCard(leftDeck.takeCard());   // takeCard uses wait() and can throw InterruptedException
@@ -188,15 +189,15 @@ public class Player implements Runnable
             }
             catch(InterruptedException e){ // thrown from takeCard (caught here so we can stop the  player
                                            // straight away, rather than have it exit takeCard() and try to
-                                           // carry out the remainder the run method.
+                                           // carry out the remainder of the run method.
                 break;
             }
         }
          ALL_ALIVE.set(false);
          
-         // below: tell the rightDeck that game has been interupted, if a thread is waiting for
+         // below: tell the rightDeck that game has been interrupted, if a thread is waiting for
          // this deck to have cards placed on it, it will not stop waiting and throw its own 
-         // interruptedException         
+         // nterruptedException         
          rightDeck.gameInterruption();
          System.out.println("Player " + preference + " exiting run");
     }
